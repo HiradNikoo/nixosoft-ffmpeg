c5ce29a6be33e4654ef68fba3ca164107ab42ba7
Hirad <hirad.nicol@gmail.com>
2018-06-06 15:37:18 +0430

    fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 process.env['ffmpeg_path'] = process.env['ffmpeg_path'] || 'E:\\Hirad\\API And Components\\FFMpeg\\ffmpeg-79465_x64\\ffmpeg\\bin\\ffmpeg.exe';
 
 const ffmpeg = require('./lib/ffmpeg');
-
 // ffmpeg.streamVideo('D:\\Video\\Politics\\Iranian Foreign Minister in Conversation with Charlie Rose.mp4').catch(function(err){
 //     console.log(err);
 // });
