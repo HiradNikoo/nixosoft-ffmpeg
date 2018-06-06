927d022139a8087f8f5d0371d312bc6e0d422f5f
Hirad <hirad.nicol@gmail.com>
2018-06-06 20:29:47 +0430

    push

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 process.env['ffmpeg_path'] = process.env['ffmpeg_path'] || 'E:\\Hirad\\API And Components\\FFMpeg\\ffmpeg-79465_x64\\ffmpeg\\bin\\ffmpeg.exe';
 
 const ffmpeg = require('./lib/ffmpeg');
+
 // ffmpeg.streamVideo('D:\\Video\\Politics\\Iranian Foreign Minister in Conversation with Charlie Rose.mp4').catch(function(err){
 //     console.log(err);
 // });
 
-ffmpeg.getCodecs().then(function(data){
+// ffmpeg.getCodecs().then(function(data){
+//     console.log(data);
+// });
+
+// ffmpeg.getFormats().then(function(data){
+//     console.log(data);
+// });
+
+ffmpeg.streamToWebM('E:\\CAMERA\\All\\MVI_3344.MOV').then(function(data){
     console.log(data);
+}).catch(function(err){
+    console.log(err);
 });
+
